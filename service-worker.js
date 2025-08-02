08bc34c3f4cc845ea8772872595af0232e51d5fd
L8SAM <noah.kost@hotmail.com>
2025-08-02 22:54:59 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,40 @@
+const CACHE_NAME = "delulu-cache-v2";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/app.js",
+  "/logo.png",
+  "/manifest.json"
+];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("delulu-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./logo-512x512.png",
-        "./delulu-favicon-512x512.png"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
 });
 
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
 self.addEventListener("fetch", event => {
+  if (event.request.mode !== "navigate") return;
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    fetch(event.request).catch(() => {
+      return caches.open(CACHE_NAME).then(cache => cache.match("/index.html"));
     })
   );
 });
